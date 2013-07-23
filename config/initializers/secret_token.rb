79e1e1b0e06c0bd9e7322c3781ff5dbc77d54a67
Jamal <tomemuecua@gmail.com>
2013-07-23 15:58:04 +0200

    eventos commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EventosApp::Application.config.secret_token = '6897a6bfb97e6b8b9fc235d430d60fcdbb4e0e8d499c21c4991694cc835fb23c777b8a791a9bc8861aa4d6e8789cafe4cb46f10f8a17f4684ecc97c549acf80c'
